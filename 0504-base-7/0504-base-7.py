5c446a245d020b7239f67b23e4a7e8e38fe32a1f
Garv <garvgogna1606@gmail.com>
2026-02-18 18:35:19 +0530

    Time: 0 ms (100%), Space: 12.4 MB (86.82%) - LeetHub

---
+++ 0504-base-7/0504-base-7.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def convertToBase7(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        if num == 0:
+            return "0"
+        
+        negative = num < 0
+        num = abs(num)
+        
+        result = ""
+        
+        while num > 0:
+            remainder = num % 7
+            result = str(remainder) + result
+            num //= 7
+        
+        if negative:
+            result = "-" + result
+        
+        return result
+        
